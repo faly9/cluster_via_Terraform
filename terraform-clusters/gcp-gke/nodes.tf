0dd6cb05a20398c3bda74643b7135e1cd9e2d172
faly9 <randriantsoafaly8@gmail.com>
2025-12-09 23:36:58 +0300

    h

--- terraform-clusters/gcp-gke/nodes.tf
+++ terraform-clusters/gcp-gke/nodes.tf
@@ -28,3 +28,37 @@ resource "google_container_node_pool" "default_pool" {
     google_container_cluster.gke_cluster
   ]
 }
+
+# ------------------------------
+# Node Pool secondaire (plus grand disque et machine)
+# ------------------------------
+resource "google_container_node_pool" "secondary_pool" {
+  name       = "${var.cluster_name}-secondary-pool"
+  cluster    = google_container_cluster.gke_cluster.name
+  location   = var.zone
+
+  node_count = 1
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    machine_type = "e2-standard-4"  # plus CPU/RAM
+    disk_size_gb = 100               # disque plus grand
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  depends_on = [
+    google_container_cluster.gke_cluster
+  ]
+}
