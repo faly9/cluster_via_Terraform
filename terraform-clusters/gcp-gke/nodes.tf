9f49102ea8f6ed1da7349cbd329d0cefd155c28c
faly9 <randriantsoafaly8@gmail.com>
2025-12-09 23:58:12 +0300

    test

--- terraform-clusters/gcp-gke/nodes.tf
+++ terraform-clusters/gcp-gke/nodes.tf
@@ -17,41 +17,7 @@ resource "google_container_node_pool" "default_pool" {
 
   node_config {
     machine_type = var.machine_type
-    disk_size_gb = var.disk_size
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-
-  depends_on = [
-    google_container_cluster.gke_cluster
-  ]
-}
-
-# ------------------------------
-# Node Pool secondaire (plus grand disque et machine)
-# ------------------------------
-resource "google_container_node_pool" "secondary_pool" {
-  name       = "${var.cluster_name}-secondary-pool"
-  cluster    = google_container_cluster.gke_cluster.name
-  location   = var.zone
-
-  node_count = 1
-
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 3
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "e2-standard-4"  # plus CPU/RAM
-    disk_size_gb = 100               # disque plus grand
+    disk_size_gb = 50  # <-- Limite de disque par nœud
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
